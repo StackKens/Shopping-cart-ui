e3133f2496b663282fde3a51a021fb1939f69e9d
alex Kochens <ofwonoalex256@gmail.com>
2026-02-12 14:13:19 +0300

    feat: fetch products and long in console

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from 'react';
+
 const App = () => {
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const res = await fetch('http://localhost:5000/products');
+      try {
+        if (!res.ok) throw new Error('OOPs Something went Wrong!');
+        const data = await res.json();
+        console.log(data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchProducts();
+  }, []);
+
   return (
     <h1 className='text-2xl text-center p-4 text-neutral-900 font-bold'>
-      🛒 Shopping Cart UI
+      🛒 Shopping Cart
     </h1>
   );
 };
